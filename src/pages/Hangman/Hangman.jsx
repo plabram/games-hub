015b408b2af4b981f5cd10d70b01281bec8a6820
Penelope <freyalabram@gmail.com>
2023-07-31 16:00:24 +0200

    v1 of Hangman. Only one letter at a time can be used.

--- src/pages/Hangman/Hangman.jsx
+++ src/pages/Hangman/Hangman.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Hangman = () => {
 
   const words = ["diplomatic", "health", "thaw", "victory", "casualty", "union", "year", "pension", "assumption", "texture"]
+  const [clue, setClue] = useState("")
+  const [randomWord, setRandomWord] = useState("")
+
+  const initGame = () => {
+    const random = words[Math.round(Math.random() * words.length)]
+    setRandomWord(random)
+    setClue(random.split("").map(i => "_").join(" ").trim())
+  }
+
+  // useEffect(() => {
+  //   console.log(clue)
+  // }, [clue])
+
+  const checkLetter = (e) => {
+    e.preventDefault()
+    console.log(randomWord, e.target.elements[0].value)
+
+    const findAndReplaceClueLetters = (letter, word) => {
+      const newClue = []
+      for (let i = 0; i < word.length; i++) {
+        (word[i] === letter) ? newClue.push(word[i]) : newClue.push("_")
+      }
+
+      return newClue.join(" ").trim()
+    }
+    const nextClue = findAndReplaceClueLetters(e.target.elements[0].value, randomWord)
+    setClue(nextClue)
+    e.target.elements[0].value = ""
+
+  }
+
   return (
-    <div>Hangman</div>
+    <>
+      <button onClick={initGame}>Start</button>
+      <p>{randomWord}</p>
+      <p>{clue}</p>
+      <form onSubmit={checkLetter}>
+        <input placeholder="Add a letter" />
+        <button type="submit">Go</button>
+      </form>
+    </>
   )
 }
 
