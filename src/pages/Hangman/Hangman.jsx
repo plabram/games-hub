0fe7376c8a6ed5421aa148f1ff06721d71b4b4ed
Penelope <freyalabram@gmail.com>
2023-07-31 17:36:15 +0200

    Initial version of hangman complete

--- src/pages/Hangman/Hangman.jsx
+++ src/pages/Hangman/Hangman.jsx
@@ -5,44 +5,59 @@ const Hangman = () => {
   const words = ["diplomatic", "health", "thaw", "victory", "casualty", "union", "year", "pension", "assumption", "texture"]
   const [clue, setClue] = useState("")
   const [randomWord, setRandomWord] = useState("")
+  const [lives, setLives] = useState(5)
 
   const initGame = () => {
     const random = words[Math.round(Math.random() * words.length)]
     setRandomWord(random)
-    setClue(random.split("").map(i => "_").join(" ").trim())
+    setClue(random.split("").map(i => "_")
+      // .join(" ").trim()
+    )
   }
-
-  // useEffect(() => {
-  //   console.log(clue)
-  // }, [clue])
 
   const checkLetter = (e) => {
     e.preventDefault()
-    console.log(randomWord, e.target.elements[0].value)
-
-    const findAndReplaceClueLetters = (letter, word) => {
+    const findAndReplaceLetters = (letter, word, prevClue) => {
       const newClue = []
       for (let i = 0; i < word.length; i++) {
-        (word[i] === letter) ? newClue.push(word[i]) : newClue.push("_")
+        if (word[i] === letter) {
+          newClue.push(word[i])
+        } else if (prevClue[i] !== "_") {
+          newClue.push(prevClue[i])
+        } else { newClue.push("_") }
+
       }
-
-      return newClue.join(" ").trim()
+      return newClue
+      // .join(" ").trim()
     }
-    const nextClue = findAndReplaceClueLetters(e.target.elements[0].value, randomWord)
-    setClue(nextClue)
-    e.target.elements[0].value = ""
 
+    if (randomWord.includes(e.target.elements[0].value)) {
+      const nextClue = findAndReplaceLetters(e.target.elements[0].value, randomWord, clue)
+      setClue(nextClue)
+    } else {
+      setLives(lives - 1)
+      if (lives == 0) { alert("Sorry! You lost.") }
+      else {
+        alert("That letter doesn't exist")
+      }
+    }
+    e.target.elements[0].value = ""
   }
+
+  useEffect(() => {
+    if (!clue.includes("_") && (clue !== "")) { alert("You won!") }
+  }, [clue])
 
   return (
     <>
       <button onClick={initGame}>Start</button>
-      <p>{randomWord}</p>
+      {/* <p>{randomWord}</p> */}
       <p>{clue}</p>
       <form onSubmit={checkLetter}>
         <input placeholder="Add a letter" />
         <button type="submit">Go</button>
       </form>
+      <p>{(lives >= 0) ? `Lives remaining: ${lives}` : "You lose"}</p>
     </>
   )
 }
