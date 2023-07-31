9cbea17c4ed53f710ca8f0084f33394fa8eb8e10
Penelope <freyalabram@gmail.com>
2023-07-31 10:01:12 +0200

    Add possibility of drawing a match

--- src/pages/Hangman/Hangman.jsx
+++ src/pages/Hangman/Hangman.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const Hangman = () => {
+
+  const words = ["diplomatic", "health", "thaw", "victory", "casualty", "union", "year", "pension", "assumption", "texture"]
   return (
     <div>Hangman</div>
   )
--- src/pages/TicTacToe/TicTacToe.jsx
+++ src/pages/TicTacToe/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const TicTacToe = () => {
 
@@ -10,7 +10,6 @@ const TicTacToe = () => {
 
   const [isStarted, setIsStarted] = useState(false)
   const [turn, setTurn] = useState("X")
-  const [totalTurns, setTotalTurns] = useState(1)
 
   const play = (a, b, player) => {
     const tempTiles = [...tiles]
@@ -18,56 +17,53 @@ const TicTacToe = () => {
     setTiles(tempTiles)
   }
 
-  const playRandomIfAvailable = (arr) => {
-    const arrFlat = arr.flat()
+  const computerPlays = () => {
+    const arrFlat = tiles.flat()
     const n = Math.round(Math.random() * arrFlat.length)
-    if (arrFlat[n] !== null) { playRandomIfAvailable(arr) }
+    if (arrFlat[n] !== null) { computerPlays() }
     else {
       const i = Math.floor(n / 3)
       const j = n % 3
       play(i, j, "O")
       setTurn("X")
-      console.log(totalTurns)
     }
   }
 
-  const finishGame = (user) => {
-    if (tiles[0][0] === user && tiles[0][1] === user && tiles[0][2] === user ||
-      tiles[1][0] === user && tiles[1][1] === user && tiles[1][2] === user ||
-      tiles[2][0] === user && tiles[2][1] === user && tiles[2][2] === user ||
-      tiles[0][0] === user && tiles[1][0] === user && tiles[2][0] === user ||
-      tiles[0][1] === user && tiles[1][1] === user && tiles[2][1] === user ||
-      tiles[0][2] === user && tiles[1][2] === user && tiles[2][2] === user ||
-      tiles[0][0] === user && tiles[1][1] === user && tiles[2][2] === user ||
-      tiles[0][2] === user && tiles[1][1] === user && tiles[2][0] === user
-    )
-      return true
-    else return false
-  }
+  useEffect(() => {
+
+    const noNullTiles = !tiles.some((row) => (row.some(i => i === null)))
+    const finishGame = (user) => {
+      if (tiles[0][0] === user && tiles[0][1] === user && tiles[0][2] === user ||
+        tiles[1][0] === user && tiles[1][1] === user && tiles[1][2] === user ||
+        tiles[2][0] === user && tiles[2][1] === user && tiles[2][2] === user ||
+        tiles[0][0] === user && tiles[1][0] === user && tiles[2][0] === user ||
+        tiles[0][1] === user && tiles[1][1] === user && tiles[2][1] === user ||
+        tiles[0][2] === user && tiles[1][2] === user && tiles[2][2] === user ||
+        tiles[0][0] === user && tiles[1][1] === user && tiles[2][2] === user ||
+        tiles[0][2] === user && tiles[1][1] === user && tiles[2][0] === user
+      )
+        return true
+      else return false
+    }
+    if (finishGame("X")) { alert("You win") }
+    if (finishGame("O")) { alert("The computer wins") }
+    if (!finishGame("X") && !finishGame("O") && noNullTiles) { alert("It's a draw") }
+  }, [tiles])
+
+
 
   const clickHandler = (i, j) => {
-    if (tiles[i][j] === null && turn === "X" && totalTurns < 5) {
-      setTotalTurns(totalTurns + 1)
+    if (tiles[i][j] === null && turn === "X") {
       play(i, j, "X")
-      finishGame("X") ? alert("You win") :
-        setTurn("O")
-      playRandomIfAvailable(tiles)
-      if (finishGame("O")) {
-        alert("The computer wins")
-      }
+      setTurn("O")
+      computerPlays()
     }
-    else if (totalTurns >= 5) {
-      finishGame("X") ? alert("You win") : alert("It's a draw")
-    }
-
-
     else if (turn !== "X") {
       alert("The computer is playing.")
     }
     else {
       alert("Someone has already used that tile.")
     }
-
   }
 
 
