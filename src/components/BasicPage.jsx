65c3e1bb9ecfb9c1a6b1c02ba0e6442543eebaba
Penelope <freyalabram@gmail.com>
2023-08-02 16:44:15 +0200

    Refactor: remove BasicPage template

--- src/components/BasicPage.jsx
+++ src/components/BasicPage.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+// import React from 'react'
 
-const BasicPage = ({ title, description }) => {
-  return (
-    <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
-    </div>
-  )
-}
+// const BasicPage = ({ title, description }) => {
+//   return (
+//     <div>
+//       <h2>{title}</h2>
+//       <p>{description}</p>
+//     </div>
+//   )
+// }
 
-export default BasicPage
+// export default BasicPage
